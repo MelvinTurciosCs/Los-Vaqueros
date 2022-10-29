8c83831c28ac462afa1df39403764f54eb5b93cc
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2022-10-29 15:56:19 -0500

    Moved Stuff

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './src/App';
+import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
