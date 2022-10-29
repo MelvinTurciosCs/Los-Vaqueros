3413872af67da37d70f513d91afc8563a3ec52ad
MelvinTurciosCs <89433567+MelvinTurciosCs@users.noreply.github.com>
2022-10-29 15:46:44 -0500

    Tried to fix error

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+import App from './src/App';
+import { ContextProvider } from './contexts/ContextProvider';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
